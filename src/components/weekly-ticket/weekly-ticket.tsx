dc0ef77490219159f1f340654342a8f8dea04fd7
kojesung <js95112345@gmail.com>
2025-02-01 22:55:37 +0900

    style : 가운데 정렬 및 사이즈

--- src/components/weekly-ticket/weekly-ticket.tsx
+++ src/components/weekly-ticket/weekly-ticket.tsx
@@ -44,24 +44,26 @@ export default function WeeklyTicke() {
         <div className="relative w-full">
             <div className="flex justify-between">
                 <button onClick={handlePrev}>{'<'}</button>
-                <Swiper
-                    modules={[Autoplay]}
-                    autoplay={{ delay: 3000 }}
-                    loop={true}
-                    slidesPerView={1}
-                    onSwiper={setSwiper}
-                >
+                <Swiper modules={[Autoplay]} autoplay={{ delay: 3000 }} slidesPerView={1} onSwiper={setSwiper}>
                     {createGroups().map((group, index) => (
                         <SwiperSlide key={index}>
                             <div className="grid grid-cols-2 gap-x-[1.527vw] gap-y-[20px]">
                                 {group.map((item) => (
                                     <div
                                         key={item.id}
-                                        className="p-[20px] flex bg-white w-[39.93vw] rounded-[20px] border-[1px] border-[rgba(240, 240, 240, 1)] h-[240px]"
+                                        className="p-[20px] flex bg-white w-[39.93vw] rounded-[20px] border-[1px] border-[rgba(240, 240, 240, 1)] h-[240px] justify-between items-center"
                                     >
                                         <Image src={item.url} alt={item.id} />
-                                        <div>{item.id}</div>
-                                        <div className="py-[21.5px]">설명</div>
+                                        <div className="justify-center items-center">{item.id}</div>
+                                        <div className="py-[21.5px] w-[20.27vw] justify-between h-[157px] items-center">
+                                            <div className="h-[38px] w-8.95vw">팬클럽선예매</div>
+                                            <div className="">날짜</div>
+                                            <div className="h-[70px]">
+                                                <div>제목</div>
+                                                <div>기간</div>
+                                                <div>기타정보</div>
+                                            </div>
+                                        </div>
                                     </div>
                                 ))}
                             </div>
