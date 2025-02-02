6dfc3b0d9acd7931b865b1f36eab3838133f6394
kojesung <js95112345@gmail.com>
2025-02-03 02:25:31 +0900

    feat : 내한/국내 콘서트 옵션 버튼

--- src/app/concert/page.tsx
+++ src/app/concert/page.tsx
@@ -12,16 +12,45 @@ async function getConcertInfo(page: number) {
 
 export default function ConcertPage() {
     const [pageNumber, setPageNumber] = useState<number>(1);
+    const [isSelectedNC, setIsSelectedNC] = useState<boolean>(false);
+    const [isSelectedKC, setIsSelectedKC] = useState<boolean>(false);
     const { data, isLoading, isError } = useQuery({
         queryKey: ['pagenatedNum', pageNumber],
-        queryFn: () => getConcertInfo(pageNumber),
+        queryFn: () => getConcertInfo(pageNumber), // 실제 API 호출에는 isSelectedNC, isSelectKC, 드롭다운 정렬 기준도 포함되어야 할듯
     });
 
-    if (isLoading) return <div>...로딩중</div>;
+    if (isLoading) return <div>로딩중...</div>;
     if (isError) return <div>에러</div>;
     return (
         <div className="px-[120px] flex flex-col">
-            <div className="px-[20px]">옵션 들어갈 칸</div>
+            <div className="px-[20px] flex justify-between  my-[30px]">
+                <div className="flex gap-[10px]">
+                    <button
+                        onClick={() => {
+                            setIsSelectedNC((prev) => !prev);
+                            setPageNumber(1);
+                        }}
+                        className={`w-[7.56vw] h-[38px] rounded-[100px] ${
+                            isSelectedNC ? 'bg-primary text-white' : 'bg-[#F7F7F7] text-[#AEAEAE]'
+                        }`}
+                    >
+                        내한 콘서트
+                    </button>
+                    <button
+                        onClick={() => {
+                            // 처음에는 onClick={(prev) => setIsSelectedNC(!prev)} 이런 실수를 했는데 여기서의 prev는 onClick 함수의 매개변수로 React의 이벤트 객체, 이정 상태값 가져오려면 setState의 매개변수에 접근해야했음
+                            setIsSelectedKC((prev) => !prev);
+                            setPageNumber(1);
+                        }}
+                        className={`w-[7.56vw] h-[38px] rounded-[100px] ${
+                            isSelectedKC ? 'bg-primary text-white' : 'bg-[#F7F7F7] text-[#AEAEAE]'
+                        }`}
+                    >
+                        국내 콘서트
+                    </button>
+                </div>
+                <div>드롭다운</div>
+            </div>
             <div className="grid grid-cols-5 gap-y-[30px] gap-x-[0.55vw]">
                 {data.concerts.map((ticket: Concert) => (
                     <Ticket key={ticket.id} concert={{ id: ticket.id, url: ticket.url, tag: ticket.tag }}></Ticket>
