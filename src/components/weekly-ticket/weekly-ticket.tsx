dd58a588a8c8821f1fb7cf3986f21ede67cb1e22
kojesung <js95112345@gmail.com>
2025-02-02 15:47:55 +0900

    style : 금주 인기 티켓 컴포넌트 디자인

--- src/components/weekly-ticket/weekly-ticket.tsx
+++ src/components/weekly-ticket/weekly-ticket.tsx
@@ -43,7 +43,12 @@ export default function WeeklyTicke() {
     return (
         <div className="relative w-full">
             <div className="flex justify-between">
-                <button onClick={handlePrev}>{'<'}</button>
+                <button
+                    onClick={handlePrev}
+                    className="absolute left-[-2%] top-[50%] -translate-y-1/2 w-[50px] h-[50px] bg-white shadow-[0_0_10px_rgba(99,99,99,0.2)] rounded-[50%] z-50"
+                >
+                    {'<'}
+                </button>
                 <Swiper modules={[Autoplay]} autoplay={{ delay: 3000 }} slidesPerView={1} onSwiper={setSwiper}>
                     {createGroups().map((group, index) => (
                         <SwiperSlide key={index}>
@@ -53,15 +58,33 @@ export default function WeeklyTicke() {
                                         key={item.id}
                                         className="p-[20px] flex bg-white w-[39.93vw] rounded-[20px] border-[1px] border-[rgba(240, 240, 240, 1)] h-[240px] justify-between items-center"
                                     >
-                                        <Image src={item.url} alt={item.id} />
+                                        <Image
+                                            src={item.url}
+                                            alt={item.id}
+                                            className="bg-black h-[200px] w-[10.41vw] rounded-[10px]"
+                                        />
                                         <div className="justify-center items-center">{item.id}</div>
-                                        <div className="py-[21.5px] w-[20.27vw] justify-between h-[157px] items-center">
-                                            <div className="h-[38px] w-8.95vw">팬클럽선예매</div>
-                                            <div className="">날짜</div>
+                                        <div className="w-[20.27vw] flex flex-col justify-between h-[157px]">
+                                            <div className="h-[38px] w-[8.95vw] border-[2px] rounded-[10px] border-primary justify-center flex items-center">
+                                                <Image
+                                                    src={test1}
+                                                    alt="logo"
+                                                    className="w-[20px] h-[20px] mr-[7px]"
+                                                ></Image>
+                                                <span className="text-[15px] font-[700] text-primary">
+                                                    멤버십 선예매
+                                                </span>
+                                            </div>
+                                            <div className="text-[20px] font-[700] text-primary">
+                                                2025.01.14 (화) 8PM
+                                            </div>
                                             <div className="h-[70px]">
-                                                <div>제목</div>
-                                                <div>기간</div>
-                                                <div>기타정보</div>
+                                                <div className="text-black text-[18px] font-[700]">{`j-hope Tour 'HOPE ON THE STAG`}</div>
+                                                {/* 글자 몇 이상 넘으면 ...처리 */}
+                                                <div className="text-[15px] font-500 text-[#AEAEAE]">
+                                                    2025.02.28 - 03.02
+                                                </div>
+                                                <div className="text-[15px] font-500 text-[#AEAEAE]">KSPO DOME</div>
                                             </div>
                                         </div>
                                     </div>
@@ -70,7 +93,12 @@ export default function WeeklyTicke() {
                         </SwiperSlide>
                     ))}
                 </Swiper>
-                <button onClick={handleNext}>{'>'}</button>
+                <button
+                    onClick={handleNext}
+                    className="absolute right-[-2%] top-[50%] -translate-y-1/2 w-[50px] h-[50px] bg-white shadow-[0_0_10px_rgba(99,99,99,0.2)] rounded-[50%] z-50"
+                >
+                    {'>'}
+                </button>
             </div>
         </div>
     );
