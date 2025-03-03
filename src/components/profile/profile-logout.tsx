f7ecf9d513aee0618aac6b8e47f6828e6f77f287
kojesung <js95112345@gmail.com>
2025-03-04 00:41:10 +0900

    style : 로그아웃 일 때 로그인 칸 height 깨짐 해결

--- src/components/profile/profile-logout.tsx
+++ src/components/profile/profile-logout.tsx
@@ -7,7 +7,7 @@ export default function ProfileLogout() {
         router.push('/login');
     };
     return (
-        <div className="rounded-[20px] bg-white w-[19.58vw] h-[11.73vw] p-[18px]">
+        <div className="rounded-[20px] bg-white w-[19.58vw] h-[fit] p-[18px]">
             <div className="flex justify-center py-[12px] mb-[8px]">
                 <span className="px-[0.83vw] whitespace-pre-line text-center">{`로그인하고 볼래말래의 
             모든 기능을 사용해 보세요`}</span>
